ffb6eef5d094fe480c8a035c9cee0cba901feb35
simon louvet <simon.louvet@imie.fr>
2014-12-02 17:14:36 +0100

    correction tp3

--- TPJDBC/src/fr/imie/tpjdbc/Launcher.java
+++ TPJDBC/src/fr/imie/tpjdbc/Launcher.java
@@ -6,6 +6,8 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /**
@@ -40,55 +42,70 @@ public class Launcher {
 			connection = DriverManager.getConnection(
 					"jdbc:postgresql://localhost:5432/imie", "postgres",
 					"postgres");
-			statement = connection.createStatement();
-			resultSet = statement
-					.executeQuery("select nom, prenom from personne");
-			while (resultSet.next()) {
-				System.out.format("%s | %s\n", resultSet.getString("nom"),
-						resultSet.getString("prenom"));
-			}
-
-			statement.close();
-			resultSet.close();
-
-			// TP 2.1
-			Boolean goodInput = false;
-			Integer input = null;
-			while (!goodInput) {
-				try {
-					System.out.println("saisir l'id d'une personne");
-					String rawInput = scanner.nextLine();
-					input = Integer.valueOf(rawInput);
-					goodInput=true;
-				} catch (NumberFormatException e) {
-					System.out.println("mauvais format");
+			Boolean endAppli = false;
+			while(!endAppli){
+				statement = connection.createStatement();
+				resultSet = statement
+						.executeQuery("select id, nom, prenom from personne");
+				Integer numLigne=1;
+				List<Integer> idPersonnes = new ArrayList<Integer>();
+				while (resultSet.next()) {
+					idPersonnes.add(resultSet.getInt("id"));
+					System.out.format("%d : %s | %s\n", numLigne++, resultSet.getString("nom"),
+							resultSet.getString("prenom"));
 				}
+
+				statement.close();
+				resultSet.close();
+
+				// TP 2.1
+				Boolean goodInput = false;
+				Integer input = null;
+				while (!goodInput) {
+					try {
+						System.out.println("saisir le numero de ligne d'une personne");
+						String rawInput = scanner.nextLine();
+						input = Integer.valueOf(rawInput);
+						goodInput=true;
+					} catch (NumberFormatException e) {
+						System.out.println("mauvais format");
+					}
+				}
+
+				if(input==0){
+					endAppli=true;
+					break;
+				}
+				//récupérer l'ID dans le tableau de conservation des id à partir de la saisie du numéro de ligne
+				String query = "select nom, prenom from personne where id="
+						.concat(idPersonnes.get(input-1).toString());
+				statement = connection.createStatement();
+				resultSet = statement.executeQuery(query);
+				while (resultSet.next()) {
+					System.out.format("%s | %s\n", resultSet.getString("nom"),
+							resultSet.getString("prenom"));
+				}
+
+				resultSet.close();
+				statement.close();
+
+				// TP 2.2
+				preparedStatement = connection
+						.prepareStatement("select nom, prenom from personne where id=?");
+				preparedStatement.setInt(1, idPersonnes.get(input-1));
+				resultSet = preparedStatement.executeQuery();
+				while (resultSet.next()) {
+					System.out.format("%s | %s\n", resultSet.getString("nom"),
+							resultSet.getString("prenom"));
+				}
+
+				resultSet.close();
+				preparedStatement.close();
 			}
-
-			String query = "select nom, prenom from personne where id="
-					.concat(input.toString());
-			statement = connection.createStatement();
-			resultSet = statement.executeQuery(query);
-			while (resultSet.next()) {
-				System.out.format("%s | %s\n", resultSet.getString("nom"),
-						resultSet.getString("prenom"));
-			}
-
-			resultSet.close();
-			statement.close();
-
-			// TP 2.2
-			preparedStatement = connection
-					.prepareStatement("select nom, prenom from personne where id=?");
-			preparedStatement.setInt(1, input);
-			resultSet = preparedStatement.executeQuery();
-			while (resultSet.next()) {
-				System.out.format("%s | %s\n", resultSet.getString("nom"),
-						resultSet.getString("prenom"));
-			}
-
-			resultSet.close();
-			preparedStatement.close();
+			
+			
+			
+			
 
 		} catch (SQLException e) {
 			throw new RuntimeException("erreure applicative", e);
