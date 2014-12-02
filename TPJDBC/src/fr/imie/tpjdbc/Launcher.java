307b233e4ddef04ef887151693c2f83120a66bf9
simon louvet <simon.louvet@imie.fr>
2014-12-02 17:17:49 +0100

    javadoc Launcher

--- TPJDBC/src/fr/imie/tpjdbc/Launcher.java
+++ TPJDBC/src/fr/imie/tpjdbc/Launcher.java
@@ -22,10 +22,10 @@ public class Launcher {
 
 	/**
 	 * fonction de démarrage de l'application utilisation de JDBC pour faire les
-	 * différentes requêtes recherche des personnes en utilisant un statement +
-	 * executeQuery recherche d'une personne en construisant dynamiquement la
-	 * requête + executeQuery recherche d'une personne en utilisant un
-	 * preparedStatement + Execute Query libération des ressources dans le
+	 * différentes requêtes. recherche des personnes en utilisant un statement +
+	 * executeQuery. recherche d'une personne en construisant dynamiquement la
+	 * requête + executeQuery. recherche d'une personne en utilisant un
+	 * preparedStatement + Execute Query. libération des ressources dans le
 	 * finally
 	 * 
 	 * @param args
