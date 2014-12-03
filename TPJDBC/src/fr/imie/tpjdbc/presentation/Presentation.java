ab33e53982cdfe37000a48012dadfdd66bbef4df
simon louvet <simon.louvet@imie.fr>
2014-12-03 15:23:09 +0100

    enrichissement IHM

--- TPJDBC/src/fr/imie/tpjdbc/presentation/Presentation.java
+++ TPJDBC/src/fr/imie/tpjdbc/presentation/Presentation.java
@@ -33,16 +33,27 @@ public class Presentation implements IPresentation {
 	@Override
 	public void start() {
 
-		try {
+		// instantiation du DAO
+					IPersonneDAO personneDAO = new PersonneDAO();
+		
+		// TP1
+		Boolean endAppli = false;
+		List<PersonneDTO> personneDTOs = null;
+		while (!endAppli) {
 
-			// TP1
-			Boolean endAppli = false;
-			while (!endAppli) {
-
-				// instantiation du DAO
-				IPersonneDAO personneDAO = new PersonneDAO();
+			
+			System.out.println("-------------------------");
+			System.out.println("1 : lister tous les users");
+			System.out.println("2 : detail d'un user     ");
+			System.out.println("3 : insérer un user      ");
+			System.out.println("-------------------------");
+			
+			Integer menuNumber = inputInteger();
+			
+			switch (menuNumber) {
+			case 1:
 				// utilisation du DAO
-				List<PersonneDTO> personneDTOs = personneDAO.findAll();
+				personneDTOs = personneDAO.findAll();
 
 				Integer numLigne = 1;
 
@@ -50,21 +61,13 @@ public class Presentation implements IPresentation {
 					System.out.format("%d : %s | %s\n", numLigne++,
 							personneDTO.getNom(), personneDTO.getPrenom());
 				}
+				break;
 
-				// TP 2.1
+			case 2:
+				// TP 2
 				Boolean goodInput = false;
 				Integer input = null;
-				while (!goodInput) {
-					try {
-						System.out
-								.println("saisir le numero de ligne d'une personne");
-						String rawInput = scanner.nextLine();
-						input = Integer.valueOf(rawInput);
-						goodInput = true;
-					} catch (NumberFormatException e) {
-						System.out.println("mauvais format");
-					}
-				}
+				input = inputInteger();
 
 				if (input == 0) {
 					endAppli = true;
@@ -73,24 +76,42 @@ public class Presentation implements IPresentation {
 
 				PersonneDTO selectedPersonne = personneDTOs.get(input - 1);
 				selectedPersonne = personneDAO.findById(selectedPersonne);
-				System.out.format("nom : %s \nprenom :  %s\ndateNaiss : %s\ntel %s\n\n",
-						selectedPersonne.getNom(),
-						selectedPersonne.getPrenom(),
-						selectedPersonne.getDateNaiss(),
-						selectedPersonne.getTel());
-
+				System.out.format(
+						"nom : %s \nprenom :  %s\ndateNaiss : %s\ntel %s\n\n",
+						selectedPersonne.getNom(), selectedPersonne.getPrenom(),
+						selectedPersonne.getDateNaiss(), selectedPersonne.getTel());
+			case 3:
+				
+				//au boulot
+			default:
+				break;
 			}
+			
+			
+			
 
-		} catch (Exception e) {
-			throw new RuntimeException("erreure applicative", e);
-		} finally {
-			if (scanner != null) {
-				scanner.close();
-			}
+			
+
 		}
 
 		System.out.println("End");
 
+	}
+
+	private Integer inputInteger() {
+		Boolean goodInput=false;
+		Integer input=null;
+		while (!goodInput) {
+			try {
+				System.out.println("saisir un numero");
+				String rawInput = scanner.nextLine();
+				input = Integer.valueOf(rawInput);
+				goodInput = true;
+			} catch (NumberFormatException e) {
+				System.out.println("mauvais format");
+			}
+		}
+		return input;
 	}
 
 }
